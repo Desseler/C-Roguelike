59356146c9d7811ce3f71c687f35c5bd458e8a32
Trevor Luster <desseler4@hotmail.com>
2017-03-26 17:08:32 -0500

    Added Extern C to fix compile errors in assign1.06

--- assign1.06/io.h
+++ assign1.06/io.h
@@ -3,6 +3,10 @@
 
 typedef struct dungeon dungeon_t;
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 void io_init_terminal(void);
 void io_reset_terminal(void);
 void io_display(dungeon_t *d);
@@ -11,4 +15,8 @@ void io_queue_message(const char *format, ...);
 void io_calculate_offset(dungeon_t *d);
 void io_update_offset(dungeon_t *d);
 
+#ifdef __cplusplus
+}
+#endif
+  
 #endif
